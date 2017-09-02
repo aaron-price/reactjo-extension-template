3faf7626c1b0b63283bf770ffff42f5d1a8ce594
Aaron Price <coding.aaronp@gmail.com>
2017-09-01 18:25:32 -0700

    Fix unpack.py

--- unpack.py
+++ unpack.py
@@ -5,8 +5,7 @@ project_name = sys.argv[1]
 ext_name = sys.argv[2]
 
 fm_asset = open(os.getcwd() + '/unpack/file_manager.py', 'r').read().format(
-    project_name=project_name,
-    ext_name=fm_asset
+    ext_name=ext_name
 )
 file = open(os.getcwd() + '/helpers/file_manager.py', 'w')
 file.write(fm_asset)
